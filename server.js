056836dd0a8e9d96ee7e2263dec7576a44c4df74
yeti-detective <yeti.detective@gmail.com>
2017-01-13 21:46:45 -0500

    EUREKA!

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var glob = require('glob');
 
 var movies = [];
@@ -13,9 +14,16 @@ for (var h = 0; h < exts.length; h++){
     })
 
 }
+
 app.get('/', (req, res) => {
     res.render('index.ejs', {movies: movies});
 });
+
+app.get('/Movies/:movie', (req, res) => {
+    res.set('Content-Type', 'video/mp4');
+    var mov = fs.createReadStream(movies[req.params.movie]);
+    mov.pipe(res);
+})
 
 app.listen(8080, () => {
     console.log('listening on 8080');
